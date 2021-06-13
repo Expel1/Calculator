af70e1d3838271afa4354a8901994d9b1b92fa83
Expel1 <78724771+Expel1@users.noreply.github.com>
2021-06-13 23:03:29 +0200

    Add project files.

---
+++ Calculator/Values.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator
+{
+    public class Values
+    {
+
+        //private int _startCost;
+        public static int StartCost { get; set; }  
+
+        //private int _handlingCost;
+        public int HandlingCost { get; set; }
+
+        //private int _earn; 
+        public int Earn { get; set; }
+
+        public static int NoProfit(int startCost, int handlingCost)
+        {
+            return startCost + handlingCost;
+        }
+
+        public static int YesProfit(int startCost, int handlingCost, int earn)
+        {
+            return startCost + handlingCost + earn;
+        }
+    }
+}   
